b242a72de07d2bc12797b4b386e950184c4cde80
David Emms <david_emms@hotmail.com>
2022-01-11 20:10:00 +0000

    Add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SHOOT",
+    version="1.1.0",
+    description="Phylogenetic gene search and ortholog inference",
+    author="David Emms",
+    author_email="david_emms@hotmail.com",
+    url="https://github.com/davidemms/SHOOT",
+    packages=["shoot"],
+    install_requires=["biopython", "ete3", "numpy", "biopython", "sklearn", "scipy"],
+)
